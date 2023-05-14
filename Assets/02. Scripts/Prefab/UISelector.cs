faba2692390b04bf83d36019afefc086d4bc6164
SangminPark <37584765+MultiDCP@users.noreply.github.com>
2023-05-14 16:32:38 +0900

    Update API Documents
    Update UISelector parts of API Documents

--- Assets/02. Scripts/Prefab/UISelector.cs
+++ Assets/02. Scripts/Prefab/UISelector.cs
@@ -5,7 +5,8 @@ using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 /// <summary>
-/// 사용자가 컨트롤러 Ray를 통해 상호작용하여 씬 내부의 오브젝트들의 Unity Event를 Invoke하는 클래스.
+/// 사용자가 컨트롤러 Ray를 통해 상호작용하여 캔버스 내부의 오브젝트들의 Unity Event를 Invoke하는 클래스.
+/// 사용 시에는 각 UI 오브젝트에 스크립트 컴포넌트를 추가하고 이벤트를 지정해준다.
 /// </summary>
 public class UISelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
@@ -15,7 +16,11 @@ public class UISelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 	private bool buttonPressed = false;
 	private float pressTime = 0.0f;
 
-	public void OnPointerEnter(PointerEventData eventData)
+    /// <summary>
+    /// 사용자의 컨트롤러 Ray가 UI 오브젝트(버튼 등)로 들어왔을 때 시간을 잰다.
+	/// 버튼이 한 번이라도 눌리지 않았을 경우에만 시간을 측정한다.
+    /// </summary>
+    public void OnPointerEnter(PointerEventData eventData)
 	{
 		if (!buttonPressed)
 		{
@@ -23,13 +28,22 @@ public class UISelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandl
 		}
 	}
 
-	public void OnPointerExit(PointerEventData eventData)
+    /// <summary>
+    /// 사용자의 컨트롤러 Ray가 UI 오브젝트를 나갔을 때 기존 루틴을 중단한다.
+    /// 눌린 시간을 초기화하고 기존에 버튼이 눌린 적이 있으면 그 기록을 초기화한다.
+    /// </summary>
+    public void OnPointerExit(PointerEventData eventData)
 	{
         pressTime = 0.0f;
         buttonPressed = false;
     }
 
-	private void Update()
+    /// <summary>
+    /// Ray가 오브젝트에 들어온 시간을 측정하여 일정 시간동안 입력이 들어왔는지 체크한다.
+    /// class 내 설정된 시간이 지나면 변수로 할당된 events들을 Invoke한다.
+	/// 중복 입력을 막기 위해 버튼이 입력되었음을 기록해준다.
+    /// </summary>
+    private void Update()
 	{
 		if(!buttonPressed && pressTime > 0.0f)
 		{
