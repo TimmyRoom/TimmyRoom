51b2d135cd60a5a61f2d794f201a2e908189ad30
EunhoKang <37584805+EunhoKang@users.noreply.github.com>
2023-05-14 15:12:23 +0900

    Fix : Add Serializable

--- Assets/02. Scripts/Global Function/GameChart.cs
+++ Assets/02. Scripts/Global Function/GameChart.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+[System.Serializable]
 public class GameChart
 {
     public string ChartName;
--- Assets/02. Scripts/Global Function/Note.cs
+++ Assets/02. Scripts/Global Function/Note.cs
@@ -1,3 +1,6 @@
+using System;
+
+[System.Serializable]
 public class Note
 {
     public float time;
