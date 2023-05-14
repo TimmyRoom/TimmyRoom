48825f01f97e3f812da68089988bd69f7f46bf16
SangminPark <37584765+MultiDCP@users.noreply.github.com>
2023-05-14 16:15:19 +0900

    Modify UISelector
    Modify UISelector to use XR Interaction Toolkits' Event Handler

--- Assets/02. Scripts/Prefab/UISelector.cs
+++ Assets/02. Scripts/Prefab/UISelector.cs
@@ -2,42 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.EventSystems;
 
 /// <summary>
 /// 사용자가 컨트롤러 Ray를 통해 상호작용하여 씬 내부의 오브젝트들의 Unity Event를 Invoke하는 클래스.
 /// </summary>
-public class UISelector : MonoBehaviour
+public class UISelector : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    public float InteractionTime;
-    public UnityEvent[] UIEvents;
-    IEnumerator InteractionRoutine;
+	public float InteractionTime = 2.0f;
+	public UnityEvent[] UIEvents;
 
-    /// <summary>
-    /// class 내 설정된 시간이 지나면 할당된 events들을 Invoke한다.
-    /// </summary>
-    IEnumerator StartTimer()
-    {
-        yield return new WaitForSeconds(InteractionTime);
-		foreach(var UIEvent in UIEvents)
+	private bool buttonPressed = false;
+	private float pressTime = 0.0f;
+
+	public void OnPointerEnter(PointerEventData eventData)
+	{
+		if (!buttonPressed)
 		{
-			UIEvent?.Invoke();
+			pressTime = Time.time;
 		}
-    }
-    
-    /// <summary>
-    /// StartCoroutine으로 StartTimer 루틴을 실행하고 InteractionRoutine에 저장한다.
-    /// </summary>
-    public void GetRayCasted()
-    {
-        InteractionRoutine = StartTimer();
-        StartCoroutine(InteractionRoutine);
+	}
+
+	public void OnPointerExit(PointerEventData eventData)
+	{
+        pressTime = 0.0f;
+        buttonPressed = false;
     }
 
-    /// <summary>
-    /// StopCoroutine으로 기존 루틴을 중단한다.
-    /// </summary>
-    public void GetRayCastStopped()
-    {
-        StopCoroutine(InteractionRoutine);
-    }
+	private void Update()
+	{
+		if(!buttonPressed && pressTime > 0.0f)
+		{
+			if(Time.time - pressTime >= InteractionTime)
+			{
+				foreach(var UIEvent in UIEvents)
+				{
+					UIEvent?.Invoke();
+				}
+				buttonPressed = true;
+			}
+		}
+	}
 }
