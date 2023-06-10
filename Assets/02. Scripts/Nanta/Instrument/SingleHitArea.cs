8d073a805821b0d9b1e87732fa0c9ce162d7dd99
EunhoKang <37584805+EunhoKang@users.noreply.github.com>
2023-06-10 10:45:41 +0900

    Hit Area Delay

--- Assets/02. Scripts/Nanta/Instrument/SingleHitArea.cs
+++ Assets/02. Scripts/Nanta/Instrument/SingleHitArea.cs
@@ -10,13 +10,29 @@ public class SingleHitArea : MonoBehaviour
     /// </summary>
     public int InstrumentType;
     /// <summary>
+    /// 중복 판정을 피하기 위해 발생하는 딜레이 구간인지 판별하는 변수.
+    /// </summary>
+    bool isSleeping = false;
+    /// <summary>
     /// 해당 오브젝트의 트리거에 진입했을 때 발생하는 액션.
     /// </summary>
     public UnityAction action;
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log($"{other.transform.position} / {this.transform.position} / {Time.time}");
-        if(other.transform.position.y > this.transform.position.y + 0.5f)
+        if(isSleeping)
+        {
+            return;
+        }
         action.Invoke();
+    }
+    /// <summary>
+    /// 중복 판정을 피하기 위해 딜레이를 발생시킨다.
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator Sleep()
+    {
+        isSleeping = true;
+        yield return new WaitForSeconds(0.1f);
+        isSleeping = false;
     }
 }
